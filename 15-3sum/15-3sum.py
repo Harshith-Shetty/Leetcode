cc55803a86fe9da9ae67b6aeea994ab219f7c055
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2022-08-05 21:38:41 +0530

    Time: 3109 ms (23.89%), Space: 17 MB (97.32%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -16,23 +16,4 @@ class Solution:
                 else:
                     r -= 1
         return list(result)
-        # output= []
-        # length = len(nums)
-        # nums.sort()
-        # for x in range (length-2):
-        #     i = x+1
-        #     dict ={}
-        #     while i<length:
-        #         sum =0 - nums[i] - nums[x]
-        #         if((sum)in dict):
-        #             list = []
-        #             list.append(nums[x])
-        #             list.append(sum)
-        #             list.append(nums[i])
-        #             if(list in output):
-        #                 pass
-        #             else:
-        #                 output.append(list)
-        #         dict[nums[i]] = i
-        #         i +=1
-        # return output
+        
