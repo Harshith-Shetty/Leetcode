422f96d7cc7608ca1eea30f203e484e6ed7aff60
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2022-08-06 22:27:54 +0530

    Time: 65 ms (53.21%), Space: 14.2 MB (45.52%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        zipped_set = set(zip(s, t))
+        return len(zipped_set) == len(set(s)) == len(set(t))
+                
+                
+            
