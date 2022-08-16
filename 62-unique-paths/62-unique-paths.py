f5358c3cee2a506c3c4883267717e7846b48c0e7
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2022-08-16 13:09:19 +0530

    Time: 47 ms (57.27%), Space: 13.8 MB (74.78%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if not m or not n:
+            return 0
+        cur = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                cur[j] += cur[j-1]
+        return cur[-1]
