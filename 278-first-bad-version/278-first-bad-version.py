b43ffe81b176b9a8e54d2c6d886c582a2befaf01
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2022-08-21 22:33:21 +0530

    Time: 40 ms (70.07%), Space: 13.9 MB (62.49%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -3,25 +3,17 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-         left = 1
-         right = n
-         while left <= right:
-
-             mid = left + (right - left) // 2
-
-             if isBadVersion(mid) == False: #== if a version is not bad, answer will definitely lie after m
-                 left = mid + 1 
-
-             else: #== if a version is bad 
-
-                 #we will check (m-1)th number - if it is bad, we will do right = m - 1
-                 #if it is good, we will return answer
-                 if mid == 1:
-                     return mid
-
-                 if isBadVersion(mid-1):
-                     right = mid - 1
-
-                 else:
-                     return mid
+        left = 1
+        right = n
+        while left <= right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid) == False: 
+             left = mid + 1 
+            else:
+             if mid == 1:
+                 return mid
+             if isBadVersion(mid-1):
+                 right = mid - 1
+             else:
+                 return mid
         
