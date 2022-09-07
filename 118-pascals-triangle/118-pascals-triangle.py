c01abf73a5106613658001560e4ea31b5507f154
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2022-09-07 08:47:49 +0530

    Time: 25 ms (99.10%), Space: 14 MB (17.81%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,9 +1,9 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         List =[]
-        for x in range(numRows): # 0,1,2,3,4,5 --> n=6
+        for x in range(numRows): 
             row_list = []
-            for y in range(x+1): # 0,1,2,3,4 --> x =4 ; 5 elements / 5th row
+            for y in range(x+1): 
                 if(y == 0 or y == x):
                     row_list.append(1)
                 else:
