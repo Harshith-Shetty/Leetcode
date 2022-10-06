7a72b1dae309aef64016789b9565a92aefce3782
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2022-10-06 23:01:14 +0530

    Time: 349 ms (96.58%), Space: 14.3 MB (12.43%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.py
@@ -0,0 +1,32 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        return self.KSumClosest(nums, 3, target)
+
+    def KSumClosest(self, nums: List[int], k: int, target: int) -> int:
+        N = len(nums)
+        if N == k:
+            return sum(nums[:k])
+
+        current = sum(nums[:k])
+        if current >= target:
+            return current
+        current = sum(nums[-k:])
+        if current <= target:
+            return current
+        
+        if k == 1:
+            return min([(x, abs(target - x)) for x in nums], key = lambda x: x[1])[0]
+
+        closest = sum(nums[:k])
+        for i, x in enumerate(nums[:-k+1]):
+            if i>0 and x == nums[i-1]:
+                continue
+            current = self.KSumClosest(nums[i+1:], k-1, target - x) + x
+            if abs(target - current) < abs(target - closest):
+                if current == target:
+                    return target
+                else:
+                    closest = current
+
+        return closest
