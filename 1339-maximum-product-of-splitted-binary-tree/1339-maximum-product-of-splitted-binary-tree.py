d205cdade104f703128e9fd03f8fb7bbb1bc878e
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2022-12-10 10:26:18 +0530

    Time: 393 ms (83.94%), Space: 76.2 MB (27.98%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
@@ -6,26 +6,14 @@
 #         self.right = right
 class Solution:
     def maxProduct(self, root: Optional[TreeNode]) -> int:
-        sum_array = []      # To store all the sum of node as we iterate on the tree
-        max_product = 0       # To store all the Final Maximum Product
-        
-        def calsum(root):   # Function to calculate the sum and store it in the ArrayList
-            if not root:    # Check whether node exists or not
-                return 0
-            
+        sum_array = []            
+        def calsum(root):   
+            if not root: return 0
             sum = root.val + calsum(root.left) + calsum(root.right)     
-            # Sum of node would be equal to (=) 
-            # Sum of its value and (+) 
-            # Sum of all node upto leaf node on its left and (+)
-            # Sum of all node upto leaf node on its right.
-            
-            sum_array.append(sum)   # Append the sum in the ArrayList
+            sum_array.append(sum)
             return sum
-            
-        total = calsum(root) # Call function to calculate total sum of entire tree
-        for x in sum_array: # Iterate through all sums of nodes
-            max_product = max((total-x)*x , max_product)
-        return max_product % (10 ** 9 + 7)
+        total = calsum(root) 
+        return max((total-x)*x for x in sum_array) % (10 ** 9 + 7)
             
             
             
