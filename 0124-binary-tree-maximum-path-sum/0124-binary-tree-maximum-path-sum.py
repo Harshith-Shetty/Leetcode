13a90242618c29a3aa187c7f009d000f2ca4b6da
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2022-12-24 10:31:40 +0530

    Time: 100 ms (76.85%), Space: 21.7 MB (11.03%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -5,17 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.maxSum = float('-inf')
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        self.max_sum = float('-inf')
         def traverse(root):
-            if root:
+            if(root):
                 left = traverse(root.left)
                 right = traverse(root.right)
-                self.maxSum = max(self.maxSum,root.val, root.val + left, root.val + right, root.val + left + right)
-                return max(root.val,root.val + left,root.val + right)
+                self.max_sum = max(self.max_sum,root.val, root.val+left,root.val+right,root.val+left+right)
+                return max(root.val,root.val+left,root.val+right)
             else:
                 return 0
         traverse(root)
-        return self.maxSum
+        return self.max_sum
+            
         
