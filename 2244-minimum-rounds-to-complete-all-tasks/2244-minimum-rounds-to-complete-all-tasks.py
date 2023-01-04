d0add7aaa191f31a29218e9bf1addbcd86ef557e
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-01-04 08:20:29 +0530

    Time: 3382 ms (5.09%), Space: 28.2 MB (70.66%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -1,4 +1,4 @@
 class Solution:
     def minimumRounds(self, tasks: List[int]) -> int:
-        C=Counter(tasks).values() 
-        return sum([ceil(c/3) for c in C if c>1]) if 1 not in C else -1
+        freq = Counter(tasks).values()
+        return -1 if 1 in freq else sum((a + 2) // 3 for a in freq)
