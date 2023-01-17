6782966e685a0aee1afe7fd3fcc7193b5328e4b5
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-01-17 06:47:47 +0530

    Time: 79 ms (100.00%), Space: 14.9 MB (93.27%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -1,15 +1,17 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        cost = 0
-        for x in s:
-            if x=='0':
-                cost += 1
-        output = cost
-        for x in s: 
-            if x=='0':
-                cost -= 1 
-                output = min(output,cost)
+        num_flips = 0
+        num_ones = 0
+        
+        for num in s:
+            # If it is one, imagine we are adding it to the end. Therefor it is always satisfying monotonicity
+            if num =='1':
+                num_ones += 1
+            # Otherwise, we need to flip the 0, so increment flips. If after that, we have more flips than ones,
+            # flip the ones instead. Do this by setting num_flips to num_ones
             else:
-                cost += 1
-            
-        return output
+                num_flips += 1
+                if num_flips > num_ones:
+                    num_flips = num_ones
+                    
+        return num_flips
