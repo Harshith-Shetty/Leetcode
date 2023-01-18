3fd311108cd9f496f3a8af17ad166762de56123b
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-01-18 16:10:12 +0530

    Time: 555 ms (77.54%), Space: 19.1 MB (43.34%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -2,16 +2,14 @@ class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
         n = len(nums)
         # Assume the array is not circular and calculate max sub array sum.
-        currentSum = nums[0]
+        currentminSum = currentmaxSum = nums[0]
         maxSum = nums[0]
-        for i in range(1, n):
-            currentSum = max(nums[i], nums[i]+currentSum)
-            maxSum = max(currentSum, maxSum)
-        currentSum = nums[0]
         minSum = nums[0]
         for i in range(1, n):
-            currentSum = min(nums[i], nums[i]+currentSum)
-            minSum = min(currentSum, minSum)
+            currentmaxSum = max(nums[i], nums[i]+currentmaxSum)
+            currentminSum = min(nums[i], nums[i]+currentminSum)
+            maxSum = max(currentmaxSum, maxSum)
+            minSum = min(currentminSum, minSum)          
         circularMaxSum = sum(nums)-minSum
         if circularMaxSum == 0:
             return maxSum
