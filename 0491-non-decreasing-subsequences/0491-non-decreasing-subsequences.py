3b4085b67bdd751dd95c14b69c1cd52d9a763dd0
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-01-20 09:49:17 +0530

    Time: 241 ms (66.41%), Space: 22.3 MB (40.47%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -1,14 +1,12 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        def dfs(i, num, curr):
-            if len(curr)>=2:
-                ans.add(curr[:])
-            if i>=len(nums):
-                return
-            for j in range(i, len(nums)):
-                if nums[j]>=num:
-                    dfs(j+1, nums[j], curr+(nums[j],))
-                        
-        ans = set()
-        dfs(0, -float("inf"), ())
-        return ans
+        def combinations(idx, curArr, nums, res):
+            if len(curArr) >= 2:
+                res.add(curArr)
+            for i in range(idx, len(nums)):
+                if not curArr or nums[i] >= curArr[-1]:
+                    combinations(i+1, curArr+(nums[i],), nums, res)
+
+        res = set()
+        combinations(0, (), nums, res)
+        return res
