be3088d2ba46f41da20c827ec3abea7e343a1fa0
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-01-20 10:37:09 +0530

    Time: 235 ms (70.16%), Space: 23.6 MB (19.53%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -1,6 +1,5 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        if not nums: return nums
         curr = [[nums[0]]]
         for x in nums[1:]:
             curr += [y+[x] for y in curr if x>=y[-1]]
