cc3f54e1a9e8d6cc473d1525695fe86641f5242c
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-01-21 10:06:36 +0530

    Time: 39 ms (74.31%), Space: 13.8 MB (99.19%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,15 +1,20 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        res = []
-        self.dfs(s, 0, "", res)
-        return res
+        path = []
+        result = []
+        self.dfs(s, path, result)
+        return result
     
-    def dfs(self, s, idx, path, res):
-        if idx > 4:
-            return 
-        if idx == 4 and not s:
-            res.append(path[:-1])
-            return 
-        for i in range(1, len(s)+1):
-            if s[:i]=='0' or (s[0]!='0' and 0 < int(s[:i]) < 256):
-                self.dfs(s[i:], idx+1, path+s[:i]+".", res)
+    def dfs(self, s: str, path: List, result: List):
+        
+        if not s and len(path) == 4:
+            s = '.'.join(path[::-1])
+            result.append(s)
+            return
+        elif len(path) == 4: return 
+        else:
+            for i in range(1, min(3, len(s))+1):
+                if int(s[:i]) >= 0 and int(s[:i]) <= 255:
+                    if i > 1 and s[0]=='0': continue
+                    else: self.dfs(s[i:], [s[:i]]+path, result)
+            return
