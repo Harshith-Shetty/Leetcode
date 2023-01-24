81748e2995f541e77297ef7537690f3626c73a27
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-01-24 08:44:11 +0530

    Time: 163 ms (48.35%), Space: 14 MB (67.56%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -1,28 +1,28 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        queue = collections.deque()
         n = len(board)
-        queue.append((1, 0)) 
-        visited = set()
-        
-        def square_to_idx(i):
-            r, c = divmod(i - 1, n)
-            if r % 2 != 0:
-                c = n - c - 1
-            r = n - r - 1
-            return r, c
-        
-        while queue:
-            cur_val, path_len = queue.popleft()
-            visited.add(cur_val)
-            cur_row, cur_col = square_to_idx(cur_val)
-            if board[cur_row][cur_col] != -1:
-                cur_val = board[cur_row][cur_col]
-            if cur_val == n ** 2:
-                return path_len
-            max_move = min(cur_val + 6, n ** 2)
-            for dest_val in range(cur_val + 1, max_move + 1):
-                if dest_val not in visited:
-                    queue.append((dest_val, path_len + 1))
+        board.reverse()
 
+        def intToPos(square):
+            r = (square - 1) // n
+            c = (square - 1) % n
+            if r % 2:
+                c = n - 1 - c
+            return [r, c]
+
+        q = deque()
+        q.append([1, 0]) 
+        visit = set()
+        while q:
+            square, moves = q.popleft()
+            for i in range(1, 7):
+                nextSquare = square + i
+                r, c = intToPos(nextSquare)
+                if board[r][c] != -1:
+                    nextSquare = board[r][c]
+                if nextSquare == n * n:
+                    return moves + 1
+                if nextSquare not in visit:
+                    visit.add(nextSquare)
+                    q.append([nextSquare, moves + 1])
         return -1
