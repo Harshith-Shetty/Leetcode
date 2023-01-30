8411c98fc49c02358a7c4b615052d6d7a754a939
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-01-30 08:21:35 +0530

    Time: 36 ms (51.68%), Space: 13.9 MB (10.24%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,13 +1,19 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        t0, t1, t2 = 0, 1, 1
-        if n == 0 or n == 1:
-            return n
-        if n == 2:
+        ar = [None]*(n+1)
+        
+        if n==0:
+            return 0
+        elif n==1:
             return 1
+        elif n==2:
+            return 1
+    
+        ar[0]=0
+        ar[1]=1
+        ar[2]=1
         
-        for i in range(2, n):
-            t = t0 + t1 + t2
-            t2, t1, t0 = t, t2, t1
-        
-        return t
+        for i in range(3,n+1):
+            ar[i]=ar[i-3]+ar[i-2]+ar[i-1]
+            
+        return ar[-1] 
