a60956fb3ae4f4f5d54387c8e12b1d165878715b
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-02-02 06:37:35 +0530

    Time: 47 ms (35.69%), Space: 13.9 MB (31.27%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,10 +1,29 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        h = {ch: i for i, ch in enumerate(order)}
-        prev = list(h[ch] for ch in words[0])
-        for i in range(1, len(words)):
-            cur = list(h[ch] for ch in words[i])
-            if cur < prev:
+        dict = {}
+        i = 0
+        for letter in order:
+            dict[letter] = i
+            i+=1
+        x = 0
+        first, second = 0, 1
+        while second < len(words):
+            idx = 0
+            f = words[first]
+            s = words[second]
+            print(f, s)
+            while idx < min(len(f), len(s)):
+                
+                if dict[f[idx]] > dict[s[idx]]:
+                    return False
+                elif dict[f[idx]] < dict[s[idx]]:
+                    x = 1
+                    break
+                idx += 1
+            if x == 0 and idx < max(len(f),len(s)) and len(f)>len(s):
                 return False
-            prev = cur
+
+            first += 1 
+            second += 1
+            
         return True
