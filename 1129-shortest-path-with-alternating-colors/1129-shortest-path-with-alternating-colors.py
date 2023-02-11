86e5efd225d2eb25a6cb2ca3e027af87ea348f03
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-02-11 09:14:39 +0530

    Time: 87 ms (81.52%), Space: 14.3 MB (31.23%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,26 +1,16 @@
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
-        g = defaultdict(list)
-        soln = [-1] * n
-        for src, dst in redEdges:
-            g[src].append((dst, 0))
-        for src, dst in blueEdges:
-            g[src].append((dst, 1))
-        soln[0] = 0
-        q = deque([(0, -1)])
-        level = 0         
-        visited = set()
-        while q:
-            levLen = len(q)
-            for i in range(levLen):
-                curr, color = q.popleft()
-                if (curr, color) in visited:
-                    continue
-                visited.add((curr, color))
-                if soln[curr] == -1:
-                    soln[curr] = level
-                for x, y in g[curr]:
-                    if color != y:
-                        q.append((x,y))
-            level += 1
-        return soln
+        graph = [[[], []] for i in range(n)]
+        for startNode, endNode in redEdges: 
+            graph[startNode][0].append(endNode)
+        for startNode, endNode in blueEdges: 
+            graph[startNode][1].append(endNode)
+        res = [[0, 0]] + [[inf, inf] for i in range(n - 1)]
+        bfsQueue = [[0, 0], [0, 1]]
+        for node, color in bfsQueue:
+            for neighbor in graph[node][color]:
+                if res[neighbor][color] == inf:
+                    res[neighbor][color] = res[node][1 - color] + 1
+                    bfsQueue.append([neighbor, 1 - color])
+        return [minPath if minPath < inf else -1 for minPath in map(min, res)]
+    
