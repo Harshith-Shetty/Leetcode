a1d5849468c48a008f39803aa2ca40c0852a3011
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-02-11 09:17:17 +0530

    Time: 92 ms (61.29%), Space: 14.1 MB (73.17%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,16 +1,17 @@
 class Solution:
-    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
-        graph = [[[], []] for i in range(n)]
-        for startNode, endNode in redEdges: 
-            graph[startNode][0].append(endNode)
-        for startNode, endNode in blueEdges: 
-            graph[startNode][1].append(endNode)
-        res = [[0, 0]] + [[inf, inf] for i in range(n - 1)]
-        bfsQueue = [[0, 0], [0, 1]]
-        for node, color in bfsQueue:
-            for neighbor in graph[node][color]:
-                if res[neighbor][color] == inf:
-                    res[neighbor][color] = res[node][1 - color] + 1
-                    bfsQueue.append([neighbor, 1 - color])
-        return [minPath if minPath < inf else -1 for minPath in map(min, res)]
+        def shortestAlternatingPaths(self, numOfNodes: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+            graph = [[[], []] for i in range(numOfNodes)]
+            for start, end in redEdges: 
+                graph[start][0].append(end)
+            for start, end in blueEdges: 
+                graph[start][1].append(end)
+            distances = [[0, 0]] + [[float("inf"), float("inf")] for i in range(numOfNodes - 1)]
+            queue = [[0, 0], [0, 1]]
+            for node, color in queue:
+                for neighbor in graph[node][color]:
+                    if distances[neighbor][color] == float("inf"):
+                        distances[neighbor][color] = distances[node][1 - color] + 1
+                        queue.append([neighbor, 1 - color])
+            return [dist if dist < float("inf") else -1 for dist in map(min, distances)]
+
     
