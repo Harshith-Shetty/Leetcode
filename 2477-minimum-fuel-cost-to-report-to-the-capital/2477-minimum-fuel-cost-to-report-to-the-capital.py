2de2a1d1ffc3e7053e605aa186b7d6c91d05c5a9
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-02-12 22:18:42 +0530

    Time: 2018 ms (71.96%), Space: 162.3 MB (57.95%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -1,25 +1,18 @@
 class Solution:
     def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
         adjacency_list = defaultdict(list)
-        
         for a, b in roads:
             adjacency_list[a].append(b)
-            adjacency_list[b].append(a)
-            
-        total_fuel_cost = [0]
-        
+            adjacency_list[b].append(a)            
+        total_fuel_cost = [0]        
         def dfs(node, parent):
-            people = 1
-            
+            people = 1            
             for neighbor in adjacency_list[node]:
                 if neighbor == parent:
                     continue
-                people += dfs(neighbor, node)
-                
+                people += dfs(neighbor, node)                
             if node != 0:
-                total_fuel_cost[0] += math.ceil(people / seats)
-                
-            return people
-        
+                total_fuel_cost[0] += math.ceil(people / seats)                
+            return people        
         dfs(0, None)
         return total_fuel_cost[0]
