7cb0f7774d95f2c76c738649b2e4d2355c9e952d
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-02-24 05:42:46 +0530

    Time: 2258 ms (64.18%), Space: 21.3 MB (97.01%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.py
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.py
@@ -1,17 +1,27 @@
 class Solution:
     def minimumDeviation(self, nums: List[int]) -> int:
-        h = []
-        mi = inf
-        for v in nums:
-            if v & 1:
-                v <<= 1
-            h.append(-v)
-            mi = min(mi, v)
-        heapify(h)
-        ans = -h[0] - mi
-        while h[0] % 2 == 0:
-            x = heappop(h) // 2
-            heappush(h, x)
-            mi = min(mi, -x)
-            ans = min(ans, -h[0] - mi)
-        return ans
+        heap = []
+        min_val = float('inf')
+        min_deviation = float('inf')
+        
+        for idx in range(len(nums)):
+            if nums[idx] % 2 != 0:
+                nums[idx] *= 2
+            
+            heapq.heappush(heap, -nums[idx])
+            min_val = min(min_val, nums[idx])
+        
+        while heap:
+            max_val = heapq.heappop(heap) * -1
+            min_deviation = min(min_deviation, max_val-min_val)
+            
+            if max_val % 2 == 0:
+                max_val //= 2
+                # after dividing by two, get a number that is less than the current minimum 
+                min_val = min(min_val, max_val)
+                heapq.heappush(heap, -max_val)
+            else:
+                #If we continue further, we will find a non-maximum deviation among the pairs of numbers in the array after the transformations
+                break
+        
+        return min_deviation
