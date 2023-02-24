71e1c82b53b12c26b7d3deb6a9e1b37903e7442f
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-02-24 05:41:26 +0530

    Time: 2149 ms (68.66%), Space: 21.6 MB (73.13%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.py
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.py
@@ -1,36 +1,17 @@
 class Solution:
     def minimumDeviation(self, nums: List[int]) -> int:
-        max_odd = 0
-        for i in nums:
-            while i%2==0:i = i//2
-            max_odd = max(max_odd, i)
-            
-        # step 2: calculating the [(xi, yi)] list
-        dev = []
-        for i in nums:
-            if i%2 == 1:
-                if 2*i>max_odd:
-                    dev.append((2*i-max_odd, max_odd-i))
-                else:
-                    dev.append((float('inf'), max_odd-2*i))
-            else:
-                if i < max_odd:
-                    dev.append((float('inf'), max_odd-i))
-                else:
-                    while i%2==0 and i>max_odd:
-                        i = i//2
-                    if i < max_odd:
-                        dev.append((2*i-max_odd, max_odd-i))
-                        
-        # step 3: minimal sum of picked xi's and yi's
-        dev.sort(reverse=True)
-        if not dev: return 0
-        max_down = [0] * len(dev)
-        cur = 0
-        for i in range(len(max_down)):
-            cur = max(cur, dev[i][1])
-            max_down[i] = cur
-        mm = min(max_down[-1], dev[0][0])
-        for i in range(len(max_down)-1):
-            mm = min(mm, max_down[i]+dev[i+1][0])
-        return mm
+        h = []
+        mi = inf
+        for v in nums:
+            if v & 1:
+                v <<= 1
+            h.append(-v)
+            mi = min(mi, v)
+        heapify(h)
+        ans = -h[0] - mi
+        while h[0] % 2 == 0:
+            x = heappop(h) // 2
+            heappush(h, x)
+            mi = min(mi, -x)
+            ans = min(ans, -h[0] - mi)
+        return ans
