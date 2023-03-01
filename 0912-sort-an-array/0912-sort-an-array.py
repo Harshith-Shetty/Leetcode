040a28a01088d7ce21a6586f8a90d5ae4691dfeb
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-03-01 22:13:16 +0530

    Time: 795 ms (87.54%), Space: 27.2 MB (13.78%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,9 +1,13 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        heapq.heapify(nums)
-        result = []
-        while (nums):
-            tmp = heapq.heappop(nums)
-            result.append(tmp)
-        
-        return result
+        cnts={}
+        index=0
+        min_value,max_value=min(nums),max(nums)
+        for i in nums:
+            cnts[i]=cnts.get(i,0)+1
+        for i in range(min_value,max_value+1):
+            while cnts.get(i,0)>0:
+                nums[index]=i
+                index+=1
+                cnts[i]-=1
+        return nums
