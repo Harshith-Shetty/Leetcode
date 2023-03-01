5f3c65f40b4d6ace21d38077346fd4c4438a4f4b
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2023-03-01 22:11:30 +0530

    Time: 819 ms (86.43%), Space: 22.2 MB (57.31%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,15 +1,9 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def quicksort(nums):
-            if len(nums) <= 1: return nums
-            
-            #picking a random pivot
-            pivot = random.choice(nums)
-            less_than, equal_to, greater_than = [], [], []
-            
-            for val in nums:
-                if val < pivot: less_than.append(val)
-                elif val > pivot: greater_than.append(val)
-                else: equal_to.append(val)
-            return quicksort(less_than) + equal_to + quicksort(greater_than)
-        return quicksort(nums)
+        heapq.heapify(nums)
+        result = []
+        while (nums):
+            tmp = heapq.heappop(nums)
+            result.append(tmp)
+        
+        return result
