358deb26e89515506c83824a480df93eed84301c
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2025-03-19 08:21:44 +0530

    Time: 7 ms (75.18%), Space: 12.8 MB (25.15%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.py
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.py
@@ -6,20 +6,4 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         #Bruteforce Approach
-        answer = []
-        unique1 = set()
-        unique2 = set()
-        
-        for i in range(len(nums1)):
-            if nums1[i] not in nums2:
-                unique1.add(nums1[i])
-
-        answer.append(list(unique1))
-        
-        for j in range(len(nums2)):
-            if nums2[j] not in nums1:
-                unique2.add(nums2[j])
-
-        answer.append(list(unique2))
-
-        return answer
+        return [list(set(nums1)-set(nums2)),list(set(nums2)-set(nums1))]
