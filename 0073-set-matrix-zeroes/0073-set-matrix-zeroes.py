567eefa2e08ee0f8da94b81d40f9c8a7c3a32aa9
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2025-04-08 08:34:32 +0530

    Time: 5 ms (58.87%), Space: 13.1 MB (74.3%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,16 +1,26 @@
 class Solution(object):
     def setZeroes(self, matrix):
-        #Brute force:
-        rows = []
-        cols = []
+        #optimal solution
+        # use the 1st row and 1st colum as our indicator array.
+        cols0 = 1
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if (matrix[i][j] == 0):
-                    rows.append(i)
-                    cols.append(j)
+                if(matrix[i][j] ==0):
+                    matrix[i][0] = 0
 
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if (i in rows or j in cols):
+                    if(j!=0):
+                        matrix[0][j] = 0
+                    else:
+                        cols0 = 0
+        for i in range(1, len(matrix)):
+            for j in range(1,len(matrix[0])):
+                if (matrix[0][j] == 0 or matrix[i][0] == 0):
                     matrix[i][j] = 0
         
+        if (matrix[0][0] == 0):
+            for j in range(len(matrix[i])):
+                matrix[0][j] = 0
+        
+        if cols0 == 0:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
