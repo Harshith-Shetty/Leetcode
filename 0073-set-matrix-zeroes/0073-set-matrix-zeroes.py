01faa7d01b0364669b7bc61652b0011496ecd78c
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2025-12-29 09:40:50 +0530

    Time: 9 ms (24%), Space: 13.1 MB (41.83%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,26 +1,38 @@
 class Solution(object):
     def setZeroes(self, matrix):
-        #optimal solution
-        # use the 1st row and 1st colum as our indicator array.
-        cols0 = 1
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if(matrix[i][j] ==0):
-                    matrix[i][0] = 0
+        # optimized solution
 
-                    if(j!=0):
-                        matrix[0][j] = 0
-                    else:
-                        cols0 = 0
-        for i in range(1, len(matrix)):
-            for j in range(1,len(matrix[0])):
-                if (matrix[0][j] == 0 or matrix[i][0] == 0):
+        rows = len(matrix)
+        cols = len(matrix[0])
+        firstRowsZero = False
+        firstColZero = False
+
+        for c in range(cols):
+            if(matrix[0][c] == 0):
+                firstRowsZero = True
+        
+        for r in range(rows):
+            if(matrix[r][0] == 0):
+                firstColZero = True
+
+        for i in range(1,rows):
+            for j in range(1, cols):
+                if(matrix[i][j] == 0):
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+        
+        for i in range(1,rows):
+            for j in range(1, cols):
+                if(matrix[0][j] == 0 or matrix[i][0] == 0 ):
                     matrix[i][j] = 0
+
+        if(firstRowsZero):
+            for c in range(cols):
+                matrix[0][c] = 0
+
+        if(firstColZero):
+            for r in range(rows):
+                matrix[r][0] = 0
+
+
         
-        if (matrix[0][0] == 0):
-            for j in range(len(matrix[i])):
-                matrix[0][j] = 0
-        
-        if cols0 == 0:
-            for i in range(len(matrix)):
-                matrix[i][0] = 0
