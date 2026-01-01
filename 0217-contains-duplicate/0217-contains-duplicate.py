9c1b7e0af8ed67bde8959247bbfeb638ae996093
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2026-01-01 13:40:30 +0530

    Time: 8 ms (94.07%), Space: 26 MB (48.62%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,10 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        # Using Maps
-        mpp ={}
-        for i in range(len(nums)):
-            if nums[i] in mpp:
-                return True
-            mpp[nums[i]] = 1
+        # Using set
+        if(len(set(nums)) != len(nums)):
+            return True
         return False
