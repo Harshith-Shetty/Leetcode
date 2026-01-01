048e23ccdc6ca3757d2da6a8e0469efc24afab2c
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2026-01-01 13:39:03 +0530

    Time: 25 ms (23.1%), Space: 26.6 MB (5.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,6 +1,13 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        if(len(set(nums))!=len(nums)):
-            return True
-        else:
-            return False
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # Using Maps
+        mpp ={}
+        for i in range(len(nums)):
+            if nums[i] in mpp:
+                return True
+            mpp[nums[i]] = 1
+        return False
