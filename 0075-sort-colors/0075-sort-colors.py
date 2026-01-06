c50b7e8e067463c647743570449596456e843c95
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2026-01-06 09:51:52 +0530

    Time: 6 ms (6.95%), Space: 12.5 MB (13.46%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,25 +1,13 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+class Solution(object):
+    def sortColors(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
-        if(len(nums) == 0 or len(nums)==1):
-            return
-        start = 0
-        end = len(nums)-1
-        index = 0
-        while(index<=end and start<end):
-            if(nums[index]==0):
-                nums[index]=nums[start]
-                nums[start]=0
-                index = index + 1
-                start = start + 1
-            elif(nums[index]==2):
-                nums[index]=nums[end]
-                nums[end]=2
-                end = end - 1
-            else:
-                index = index + 1
-            
-                
+        # bubble sort
+        for i in range(len(nums)-1, -1,-1):
+            for j in range(i):
+                if(nums[j] > nums [j + 1]):
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+        return nums
         
