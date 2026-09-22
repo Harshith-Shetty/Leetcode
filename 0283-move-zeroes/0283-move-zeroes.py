abbadd0f8eaf8aeb9a5b0b334b24e1751dce11d0
Harshith Shetty <56068877+Harshith-Shetty@users.noreply.github.com>
2026-09-22 07:37:05 +0530

    Time: 11 ms (25.61%), Space: 13.5 MB (50.7%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,16 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        # using temp list O(n)
-        temp = [0] * len(nums)
+        # without using a temp array
         index = 0
-        curr = 0
 
-        while curr < len(nums):
+        for curr in range(len(nums)):
             if nums[curr] != 0:
-                temp[index] = nums[curr]
+                nums[index] = nums[curr]
                 index +=1
-            curr +=1
-        nums[:] = temp
+            
+        while index < len(nums):
+            nums[index] = 0
+            index +=1
+        
 
         
